90df98b065ccf9d32a4e923d73e1c5538421ad28
Harshad <harshadnikhare10@gmail.com>
2022-04-12 13:55:59 +0530

    sign in with node api success

--- src/Components/Admin/SignUp.js
+++ src/Components/Admin/SignUp.js
@@ -1,7 +1,102 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, Navigate, useNavigate } from "react-router-dom";
+import axios from "axios";
+import "react-toastify/dist/ReactToastify.css";
 
+import { ToastContainer, toast } from "react-toastify";
 const SignUp = () => {
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const toastOptions = {
+    position: "bottom-center",
+    pauseOnHover: true,
+    draggable: true,
+    theme: "dark",
+  };
+  const notify = () => toast("Wow so easy!");
+  const handlevalidation = () => {
+    if (password === "") {
+      toast.error("password is Required", toastOptions);
+      return false;
+    } else if (email.length === "") {
+      toast.error("email and password is required", toastOptions);
+      return false;
+    }
+    return true;
+  };
+  const SignInHandler = (event) => {
+    event.preventDefault();
+
+    if (handlevalidation()) {
+      axios
+        .post("http://localhost:5000/api/auth/login", { email, password })
+        .then(({ status, data }) => {
+          if (status === 200) {
+            alert("success");
+            toast.success("Login Succussfully");
+            localStorage.setItem("user", JSON.stringify(data));
+            // navigate("/dashboard");
+          }
+        })
+        .catch((err) => {
+          alert("catc");
+          console.log("catch", err.response.data);
+          toast.error(err.response.data.message, toastOptions);
+        });
+      // try {
+      //   const { status, data } = axios.post("http://localhost:5000/api/auth/login",{email,password})
+      //   if (status === 200) {
+      //     alert('try')
+      //     toast.success("Login Succussfully");
+      //     localStorage.setItem('user', JSON.stringify(data));
+      //     navigate("/dashboard");;
+      //   } else {
+      //     alert('else')
+      //   }
+      // } catch (err) {
+      //   alert()
+      //   console.log(err.response.data.message,"error")
+
+      // }
+    }
+    // if(handlevalidation()) {
+    //   alert('if')
+    //      axios.post("http://localhost:5000/api/auth/login",{email,password}).then((data) => {
+    //        try {
+    //        console.log(data,"data");
+
+    //          alert('try')
+    //         toast.success("Login Succussfully");
+    //         localStorage.setItem('user', JSON.stringify(data));
+    //         navigate("/dashboard");
+    //        } catch (error) {
+    //          alert()
+    //           console.log(error.response.data.message,"error")
+    //        }
+    //       //  if(data.status === 200){
+
+    //       //    toast.success("Login Succussfully");
+    //       //    localStorage.setItem('user', JSON.stringify(data));
+    //       //    navigate("/dashboard");
+    //       //  }
+    //       //  else {
+    //       //   toast.error("Invalid Credential", toastOptions);
+    //       //  }
+    //     //  }).catch((err) => {
+
+    //     //    console.log(err.response.data.message)
+    //      })
+    // }
+  };
+  //   await  axios
+  //   .post("http://localhost:5000/api/auth/login", {
+  //     email: email,
+  //     password: password,
+  //   }).then((data) =>{
+  //  console.log(data)
+  //  console.log(42,dat
   return (
     <>
       <main className="main-content  mt-0">
@@ -30,26 +125,34 @@ const SignUp = () => {
                         style={{ color: " #D81B60" }}
                       >
                         <h4 className="font-weight-bolder">Sign In</h4>
-                        <p className="mb-0">
+                        {/* <p className="mb-0">
                           Enter your email and password To Login
-                        </p>
+                        </p> */}
                       </div>
                       <div
                         className="card-body"
                         style={{ marginBottom: "50px" }}
                       >
-                        <form role="form">
-                          <div className="input-group input-group-outline mb-3">
+                        <form role="form" onSubmit={SignInHandler}>
+                          {/* <div className="input-group input-group-outline mb-3">
                             <label className="form-label">Name</label>
-                            <input type="text" className="form-control" />
-                          </div>
+                            <input type="text" className="form-control" onChange={(e) => setName(e.target.value)} />
+                          </div> */}
+                          <label className="form-label">Email</label>
                           <div className="input-group input-group-outline mb-3">
-                            <label className="form-label">Email</label>
-                            <input type="email" className="form-control" />
+                            <input
+                              type="email"
+                              className="form-control"
+                              onChange={(e) => setEmail(e.target.value)}
+                            />
                           </div>
+                          <label className="form-label">Password</label>
                           <div className="input-group input-group-outline mb-3">
-                            <label className="form-label">Password</label>
-                            <input type="password" className="form-control" />
+                            <input
+                              type="password"
+                              className="form-control"
+                              onChange={(e) => setPassword(e.target.value)}
+                            />
                           </div>
 
                           <div
@@ -62,7 +165,7 @@ const SignUp = () => {
                           </div>
                           <div className="text-center">
                             <button
-                              type="button"
+                              type="submit"
                               className="btn btn-lg bg-gradient-primary btn-lg w-100 mt-4 mb-0"
                             >
                               Admin Login
@@ -78,6 +181,7 @@ const SignUp = () => {
           </div>
         </section>
       </main>
+      <ToastContainer />
     </>
   );
 };
